c8f59b3d9dbdc8da095f7358e91b4ba9560665ce
yesh-krish <85389282+yesh-krish@users.noreply.github.com>
2026-07-04 16:11:05 +0300

    Add: perfect-squares - submission-1

---
+++ Data Structures & Algorithms/perfect-squares/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [float("inf")] * (n + 1)
+        dp[0] = 0
+
+        for i in range(1, n+ 1):
+            j = 1
+            while j * j <= i:
+                dp[i] = min(dp[i], 1 + dp[i - j * j])  
+                j += 1
+        return dp[n]      
